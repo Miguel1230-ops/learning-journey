8eddcf6be512fd5ab160a8b4ddef1c9937e02584
Miguel <miguelcuba765@gmail.com>
2026-05-21 12:16:00 -0600

    Mi primer programa en JavaScript

---
+++ mi-primer-programa.js
@@ -0,0 +1,8 @@
+// Mi primer programa
+let nombre = "Miguel";
+let edad = 15;
+let meta = "millon de dolares";
+
+console.log("Me llamo " + nombre);
+console.log("Tengo " + edad + " años");
+console.log("Mi meta es llegar al " + meta);
