b936c0eb3d43a1f5b21293eb597750720377ca95
Miguel <miguelcuba765@gmail.com>
2026-09-19 14:12:29 -0600

    limpiar marcas de conflicto en mi-primer-programa.js

--- dia1-javascript/mi-primer-programa.js
+++ dia1-javascript/mi-primer-programa.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let cliente = {
     nombre: "Carlos",
     negocio: "Restaurante El Buen Sabor",
@@ -11,7 +11,6 @@ console.log(cliente.nombre)
 console.log(cliente.negocio)
 console.log("Problema a resolver: " + cliente.problema)
 console.log("Puede pagar: $" + cliente.presupuesto)
-=======
 // Mi primer programa
 let nombre = "Miguel";
 let edad = 15;
@@ -20,4 +19,3 @@ let meta = "millon de dolares";
 console.log("Me llamo " + nombre);
 console.log("Tengo " + edad + " años");
 console.log("Mi meta es llegar al " + meta);
->>>>>>> c799ed60e68250af55286dde09dbae2783fb6493
